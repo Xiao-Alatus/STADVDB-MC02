afa00f2d95c04193d9eb505f4fbbaccacfd3bfc7
svcheng <samuel.vincent.cheng@gmail.com>
2024-04-02 23:27:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -5,4 +5,20 @@ app.get("/", (req, res)=>{
     res.send('Hello World'); 
 });
 
+app.get("/read/:id", (req, res)=>{
+    res.send('read path'); 
+});
+
+app.post("/insert/:id", (req, res)=>{
+    res.send('insert path'); 
+});
+
+app.patch("/update/:id", (req, res)=>{
+    res.send('update path'); 
+});
+
+app.delete("/delete/:id", (req, res)=>{
+    res.send('delete path'); 
+});
+
 app.listen(3000, ()=> console.log("App Listening on port 3000"));
