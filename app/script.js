56d04c9cd8d69b5df83d1671a0917bb01a14c0e1
Maracoo <marco_jalen_yu@dlsu.edu.ph>
2024-04-09 17:00:07 +0800

    All CRUD implemented

--- app/script.js
+++ app/script.js
@@ -1,6 +1,9 @@
 searchBtn = document.getElementById('searchBtn');
 createBtn = document.getElementById('createBtn');
+closeAddApptBtn = document.getElementById('closeAddApptBtn');
+closeEditApptBtn = document.getElementById('closeEditApptBtn');
 
+// Search for appointments
 searchBtn.addEventListener('click', async function() {
     // Get the search input value
     searchInput = document.getElementById('searchInput').value;
@@ -39,8 +42,12 @@ searchBtn.addEventListener('click', async function() {
     
             const deleteBtn = document.createElement('button');
             deleteBtn.textContent = 'Delete';
+            deleteBtn.addEventListener('click', deleteEventListener);
+
             const editBtn = document.createElement('button');
             editBtn.textContent = 'Edit';
+            editBtn.addEventListener('click', editEventListener);
+
             deleteCell.appendChild(deleteBtn);
             editCell.appendChild(editBtn);
         }
@@ -67,4 +74,117 @@ function clearTable() {
 // Add Appointment
 createBtn.addEventListener('click', async function() {
     document.getElementById('add-appointment-modal-container').style.display = 'block';
+});
+
+closeAddApptBtn.addEventListener('click', function() {
+    document.getElementById('add-appointment-modal-container').style.display = 'none';
+});
+
+document.getElementById('appointmentID').addEventListener('input', function() {
+    var input = this.value;
+    console.log(input)
+    var regex = /^[0-9a-fA-F]{32}$/;
+    if (regex.test(input) || input.length == 0 || input.length == 32) {
+        document.getElementById('error-message').style.display = 'none';
+        document.getElementById('addApptBtn').disabled = false;
+    } else {
+        document.getElementById('error-message').style.display = 'block';
+        document.getElementById('addApptBtn').disabled = true;
+    }
+});
+
+document.getElementById('add-appointment-form').addEventListener('submit', async function(event) {
+    // Prevent the form from submitting
+    event.preventDefault();
+    // Get the form data
+    const formData = new FormData(this);
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+    // Connect to the route
+    const response = await fetch('/add', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    // Close the modal
+    document.getElementById('add-appointment-modal-container').style.display = 'none';
+
+    window.alert('Appointment added successfully!');
+
+    // Clear the form
+    this.reset();
+});
+
+// Delete Appointment
+const deleteEventListener = async (e) => {
+    const searchInput = e.target.parentElement.parentElement.cells[0].textContent;
+    const response = await fetch('/delete', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ searchInput })
+    });
+
+    if (response.status == 200) {
+        window.alert('Appointment deleted successfully!');
+        e.target.parentElement.parentElement.remove();
+    } else {
+        window.alert('Error: Appointment not found!');
+    }
+}
+
+// Edit Appointment
+const editEventListener = async (e) => {
+    // Set the values of the edit form
+    const cells = e.target.parentElement.parentElement.cells;
+    document.getElementById('editAppointmentID').value = cells[0].textContent;
+    document.getElementById('editStatus').value = cells[1].textContent;
+    document.getElementById('editCity').value = cells[2].textContent;
+    document.getElementById('editProvince').value = cells[3].textContent;
+    document.getElementById('editRegionName').value = cells[4].textContent;
+    document.getElementById('edit-appointment-modal-container').style.display = 'block';
+}
+
+closeEditApptBtn.addEventListener('click', function() {
+    document.getElementById('edit-appointment-modal-container').style.display = 'none';
+});
+
+document.getElementById('edit-appointment-form').addEventListener('submit', async function(event) {
+    // Prevent the form from submitting
+    event.preventDefault();
+    // Get the form data
+    const formData = new FormData(this);
+    const data = {};
+    formData.forEach((value, key) => {
+        data[key] = value;
+    });
+    // Connect to the route
+    const response = await fetch('/edit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    if (response.status == 200) {
+        window.alert('Appointment edited successfully!');
+    } else {
+        window.alert('Error: Appointment not found!');
+    }
+
+    // Close the modal
+    document.getElementById('edit-appointment-modal-container').style.display = 'none';
+
+    // Search for appointments
+    document.getElementById('searchInput').value = data.apptid;
+    searchBtn.click();
+
+    // Clear the form
+    this.reset();
 });
--- routes/route.js
+++ routes/route.js
@@ -32,7 +32,47 @@ router.post("/search", async (req, res)=>{
             sql = `SELECT * FROM apptdb.appointments WHERE apptid = '${appointmentID}'`;
         }
         const result = await executeSQL(sql);
-        console.log(result)
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Add
+router.post("/add", async (req, res)=>{
+    try {
+        const { apptid, status, city, province, regionName } = req.body;
+        const sql = `INSERT INTO apptdb.appointments (apptid, status, City, Province, RegionName) VALUES ('${apptid}', '${status}', '${city}', '${province}', '${regionName}')`;
+        const result = await executeSQL(sql);
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+
+});
+
+// Delete
+router.post("/delete", async (req, res)=>{
+    try {
+        const appointmentID = req.body.searchInput
+        const sql = `DELETE FROM apptdb.appointments WHERE apptid = '${appointmentID}'`;
+        const result = await executeSQL(sql);
+        res.status(200);
+        res.end();
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: error.message });
+    }
+});
+
+// Edit
+router.post("/edit", async (req, res)=>{
+    try {
+        const { apptid, status, city, province, regionName } = req.body;
+        const sql = `UPDATE apptdb.appointments SET status = '${status}', City = '${city}', Province = '${province}', RegionName = '${regionName}' WHERE apptid = '${apptid}'`;
+        const result = await executeSQL(sql);
         res.json(result);
     } catch (error) {
         console.log(error);
