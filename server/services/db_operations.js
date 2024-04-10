0cc63248d77e01220d92aefbeaf6dca23f36219b
Maracoo <marco_jalen_yu@dlsu.edu.ph>
2024-04-10 20:02:47 +0800

    Updated the updateLogFile and syncLogFiles

--- server/services/db_operations.js
+++ server/services/db_operations.js
@@ -35,38 +35,58 @@ export async function checkConnection(){
 }
 
 //called whenever any query is executed; updates log file with the query executed
-export async function updateLogFile(serverlog){
+export async function updateLogFile(serverlog, query){
     statuses = checkConnection();
     if(serverlog === 'luzon') {
         if(statuses.main_status && statuses.luzon_status){
             syncLogFiles('luzon') //sync log files, then update main
-            
-            //update main log file
 
+            // get latest index on log table for luzon_db
+            let logFileIndex = getLogFileIndex('luzon');
+            // increment index
+            logFileIndex++;
+            //update main log file
+            await main_db.query(`INSERT INTO luzon_log (log_file_index, log_file) VALUES (${logFileIndex}, ${query})`);
             //update luzon log file
+            await luzon_db.query(`INSERT INTO luzon_log (log_file_index, log_file) VALUES (${logFileIndex}, ${query})`);
         }
         else if (statuses.main_status){
             //update main log file
+            let logFileIndex = getLogFileIndex('main_luzon');
+            logFileIndex++;
+            await main_db.query(`INSERT INTO luzon_log (log_file_index, log_file) VALUES (${logFileIndex}, ${query})`);
         }
         else if (statuses.luzon_status){
             //update luzon log file
-        
+            let logFileIndex = getLogFileIndex('luzon');
+            logFileIndex++;
+            await luzon_db.query(`INSERT INTO luzon_log (log_file_index, log_file) VALUES (${logFileIndex}, ${query})`);
         }
     }
     else if (serverlog === 'vismin') {
         if(statuses.main_status && statuses.vismin_status){
-            syncLogFiles('luzon') //sync log files, then update main
+            syncLogFiles('vismin') //sync log files, then update main
             
+            // get latest index on log table for vismin_db
+            let logFileIndex = getLogFileIndex('vismin');
+            // increment index
+            logFileIndex++;
             //update main log file
-
+            await main_db.query(`INSERT INTO vismin_log (log_file_index, log_file) VALUES (${logFileIndex}, ${query})`);
             //update vismin log file
+            await vismin_db.query(`INSERT INTO vismin_log (log_file_index, log_file) VALUES (${logFileIndex}, ${query})`);
         }
         else if (statuses.main_status){
             //update main log file
+            let logFileIndex = getLogFileIndex('main_vismin');
+            logFileIndex++;
+            await main_db.query(`INSERT INTO vismin_log (log_file_index, log_file) VALUES (${logFileIndex}, ${query})`);
         }
         else if (statuses.vismin_status){
             //update vismin log file
-        
+            let logFileIndex = getLogFileIndex('vismin');
+            logFileIndex++;
+            await vismin_db.query(`INSERT INTO vismin_log (log_file_index, log_file) VALUES (${logFileIndex}, ${query})`);
         }
     }
 }
@@ -95,12 +115,80 @@ export async function syncLogFiles(choice = "all"){
         //if luzon_db is up, sync luzon_db log file and main_db luzon log file
         if (statuses.luzon_status && (choice === "all" || choice === "luzon")){
             //sync luzon_db log file and main_db luzon log file
-            console.log("deez")
+            let luzon_log_index = null;
+            let main_luzon_log_index = null;
+            let rows = [];
+            
+            // Get latest index on log table for luzon_db and main_db
+            [rows] = getLogFileIndex('luzon');
+            if (rows.length === 0){
+                luzon_log_index = 0;
+            } else if (rows.length === 1) {
+                luzon_log_index = rows[0].id;
+            }
+            [rows] = getLogFileIndex('main_luzon');
+            if (rows.length === 0){
+                main_luzon_log_index = 0;
+            } else if (rows.length === 1) {
+                main_luzon_log_index = rows[0].id;
+            }
+
+            // If not sync, sync log files
+            if (luzon_log_index !== main_luzon_log_index){
+                //sync log files from whoever has a higher index
+                if (luzon_log_index > main_luzon_log_index){
+                    // select the luzon_db log file and insert it into main_db luzon log file
+                    [rows] = await luzon_db.query(`SELECT * FROM luzon_log WHERE id > ${main_luzon_log_index}`);
+                    for (let i = 0; i < rows.length; i++){
+                        [rows] = await main_db.query(`INSERT INTO luzon_log (id, log_entry) VALUES (${rows[i].id}, ${rows[i].log_entry})`);
+                    }
+                } else {
+                    //sync main_db luzon log file to luzon_db log file
+                    [rows] = await main_db.query(`SELECT * FROM luzon_log WHERE id > ${luzon_log_index}`);
+                    for (let i = 0; i < rows.length; i++){
+                        [rows] = await luzon_db.query(`INSERT INTO luzon_log (id, log_entry) VALUES (${rows[i].id}, ${rows[i].log_entry})`);
+                    }
+                }
+            }
         }
         //if vismin_db is up, sync vismin_db log file and main_db vismin log file
         if (statuses.vismin_status && (choice === "all" || choice === "vismin")){
             //sync vismin_db log file and main_db vismin log file
-            console.log("nuts")
+            let vismin_log_index = null;
+            let main_vismin_log_index = null;
+            let rows = [];
+
+            // Get latest index on log table for vismin_db and main_db
+            [rows] = getLogFileIndex('vismin');
+            if (rows.length === 0){
+                vismin_log_index = 0;
+            } else if (rows.length === 1) {
+                vismin_log_index = rows[0].id;
+            }
+            [rows] = getLogFileIndex('main_vismin');
+            if (rows.length === 0){
+                main_vismin_log_index = 0;
+            } else if (rows.length === 1) {
+                main_vismin_log_index = rows[0].id;
+            }
+            
+            // If not sync, sync log files
+            if (vismin_log_index !== main_vismin_log_index){
+                //sync log files from whoever has a higher index
+                if (vismin_log_index > main_vismin_log_index){
+                    // select the vismin_db log file and insert it into main_db vismin log file
+                    [rows] = await vismin_db.query(`SELECT * FROM vismin_log WHERE id > ${main_vismin_log_index}`);
+                    for (let i = 0; i < rows.length; i++){
+                        [rows] = await main_db.query(`INSERT INTO vismin_log (id, log_entry) VALUES (${rows[i].id}, ${rows[i].log_entry})`);
+                    }
+                } else {
+                    //sync main_db vismin log file to vismin_db log file
+                    [rows] = await main_db.query(`SELECT * FROM vismin_log WHERE id > ${vismin_log_index}`);
+                    for (let i = 0; i < rows.length; i++){
+                        [rows] = await vismin_db.query(`INSERT INTO vismin_log (id, log_entry) VALUES (${rows[i].id}, ${rows[i].log_entry})`);
+                    }
+                }
+            }
         }
     }
 }
